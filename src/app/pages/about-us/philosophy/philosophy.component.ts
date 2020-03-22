968d264d53bcbcfb072cfd104ee4eb28552e04bf
Mark Emrich <mark.emrich@gmx.de>
2020-03-22 15:15:32 +0100

    Fixed philosophy mb and roof for mobile

--- src/app/pages/about-us/philosophy/philosophy.component.ts
+++ src/app/pages/about-us/philosophy/philosophy.component.ts
@@ -1,3 +1,6 @@
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./philosophy.component.scss']
 })
 export class PhilosophyComponent implements OnInit {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Handset])
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit() {
   }
