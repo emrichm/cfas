2ed468c9d4d5458b2bd1cbd369d7a9e0bc60dad6
Mark Emrich <mark.emrich@gmx.de>
2020-03-31 20:38:13 +0200

    Events are externalized

--- src/app/pages/offer/events/event.ts
+++ src/app/pages/offer/events/event.ts
@@ -1,6 +1,20 @@
-export interface Event {
+export class Event {
+    date: Date;
+    description: string;
+    image: {
+        url: string;
+        alt: string;
+    }
+
+    constructor(eventDTO: EventDTO) {
+        this.date = new Date(eventDTO.date);
+        this.description = eventDTO.description;
+        this.image = eventDTO.image;
+    }
+}
+
+export interface EventDTO {
     date: string;
-    time: string;
     description: string;
     image: {
         url: string;
--- src/app/pages/offer/events/events.service.ts
+++ src/app/pages/offer/events/events.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Event } from './event';
+import * as eventDTO from '../../../../assets/data/events.json';
+import { Event, EventDTO } from './event';
 
-let events: Event[];
+let events = (eventDTO.default as EventDTO[]).map(event => new Event(event));
+events = events.filter(event => event.date.getTime() >= new Date().getTime());
+events = events.sort((a, b) => a.date.getTime() - b.date.getTime());
 
 @Injectable({
   providedIn: 'root'
@@ -11,87 +14,3 @@ export class EventsService {
     return events;
   }
 }
-
-events = [
-  {
-    date: '19.01.2020',
-    time: '10:00',
-    description: 'TeamWOD Fotoshoot',
-    image: {
-      url: 'assets/images/events/20200628-0802_CFGAMES_KINO.jpg',
-      alt: ''
-    }
-  },
-  {
-    date: '15.02.2020',
-    time: '10:00',
-    description: 'STAMMTISCH \„Weißwurst & Brez\’N\“',
-    image: {
-      url: 'assets/images/events/20200404_2terGeburtstag.jpg',
-      alt: ''
-    }
-  },
-  {
-    date: '09.03.2020',
-    time: '18:00',
-    description: 'RuderWorkshop Teil 1',
-    image: {
-      url: 'assets/images/events/20200328_stammtisch.jpg',
-      alt: ''
-    }
-  },
-  {
-    date: '16.03.2020',
-    time: '18:00',
-    description: 'RuderWorkshop Teil 2',
-    image: {
-      url: 'assets/images/events/20200425_AngeLove Nutrition.jpg',
-      alt: ''
-    }
-  },
-  {
-    date: '23.03.2020',
-    time: '18:00',
-    description: 'RuderWorkshop Teil 3',
-    image: {
-      url: 'assets/images/events/20200404_2terGeburtstag.jpg',
-      alt: ''
-    }
-  },
-  {
-    date: '28.03.2020',
-    time: '10:00',
-    description: 'STAMMTISCH „Healthy Food“ + BurpeeChallenge 1000',
-    image: {
-      url: 'assets/images/events/20200328_stammtisch.jpg',
-      alt: ''
-    }
-  },
-  {
-    date: '04.04.2020',
-    time: '10:00',
-    description: 'CROSSFIT AM SEE’s 🎁ZWEITER GEBURTSTAG🥳',
-    image: {
-      url: 'assets/images/events/20200404_2terGeburtstag.jpg',
-      alt: ''
-    }
-  },
-  {
-    date: '25.04.2020',
-    time: '10:30',
-    description: 'Talk with ALN Nutrition Workshop',
-    image: {
-      url: 'assets/images/events/20200425_AngeLove Nutrition.jpg',
-      alt: ''
-    }
-  },
-  {
-    date: '16.05.2020',
-    time: '10:00',
-    description: 'Weightlifting Workshop – Athletik Club Konstanz',
-    image: {
-      url: 'assets/images/events/20200328_stammtisch.jpg',
-      alt: ''
-    }
-  }
-]
