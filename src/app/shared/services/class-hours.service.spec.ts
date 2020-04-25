d22d0e5ad73b441d3435ffc885533de9ac151625
Mark Emrich <mark.emrich@gmx.de>
2020-04-25 23:33:48 +0200

    Added 2nd test

--- src/app/shared/services/class-hours.service.spec.ts
+++ src/app/shared/services/class-hours.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { ClassHoursService } from './class-hours.service';
 
-xdescribe('ClassHoursService', () => {
+describe('ClassHoursService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
