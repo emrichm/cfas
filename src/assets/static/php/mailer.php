fcc7daf570ec6f34483be984c0f56809d60889bb
Mark Emrich <mark.emrich@gmx.de>
2022-03-09 18:42:40 +0100

    Adapt mail address from inquiry from

--- src/assets/static/php/mailer.php
+++ src/assets/static/php/mailer.php
@@ -1,42 +1,42 @@
 <?php
 switch ($_SERVER['REQUEST_METHOD']) {
-    case ("OPTIONS"):
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Request-Headers: POST");
-        header('Access-Control-Allow-Headers: content-type');
-        exit;
-    case ("POST"):
-        header("Access-Control-Allow-Origin: *");
+  case ("OPTIONS"):
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Request-Headers: POST");
+    header('Access-Control-Allow-Headers: content-type');
+    exit;
+  case ("POST"):
+    header("Access-Control-Allow-Origin: *");
 
-        $inquiry = json_decode(file_get_contents('php://input'), true);
+    $inquiry = json_decode(file_get_contents('php://input'), true);
 
-        $firstName = $inquiry['firstName'];
-        $lastName = $inquiry['lastName'];
-        $email = $inquiry['email'];
-        $subject = $inquiry['subject'];
-        $wishDate = substr($inquiry['wishDate'], 0, 10);
-        $hour = $inquiry['hour'];
-        $message = $inquiry['message'];
+    $firstName = $inquiry['firstName'];
+    $lastName = $inquiry['lastName'];
+    $email = $inquiry['email'];
+    $subject = $inquiry['subject'];
+    $wishDate = substr($inquiry['wishDate'], 0, 10);
+    $hour = $inquiry['hour'];
+    $message = $inquiry['message'];
 
-        // Create the email and send the message
-        $myemail = 'Anna@CrossFitamSee.de';
-        $email_subject = "Webseiten-Anfrage: $subject";
-        $email_body = "Name: $firstName $lastName";
-        if ($wishDate) {
-            $email_body .= "\nWunschtermin: $wishDate";
-        }
-        if ($hour) {
-            $email_body .= " -> $hour";
-        }
-        $email_body .= "\n\nNachricht:\n$message";
-        $headers = "From: $email";
-        $headers .= "\nReply-To: $email";
+    // Create the email and send the message
+    $myemail = 'Alex@CrossFitamSee.de';
+    $email_subject = "Webseiten-Anfrage: $subject";
+    $email_body = "Name: $firstName $lastName";
+    if ($wishDate) {
+      $email_body .= "\nWunschtermin: $wishDate";
+    }
+    if ($hour) {
+      $email_body .= " -> $hour";
+    }
+    $email_body .= "\n\nNachricht:\n$message";
+    $headers = "From: $email";
+    $headers .= "\nReply-To: $email";
 
-        mail($myemail, $email_subject, $email_body, $headers);
+    mail($myemail, $email_subject, $email_body, $headers);
 
-        echo json_encode(true);
-        break;
-    default:
-        header("Allow: POST", true, 405);
-        exit;
+    echo json_encode(true);
+    break;
+  default:
+    header("Allow: POST", true, 405);
+    exit;
 }
