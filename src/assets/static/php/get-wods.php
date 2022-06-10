b71c21fbf29a590c72d032d9693360de9872a0fa
Mark Emrich <mark.emrich@gmx.de>
2022-06-10 08:13:48 +0200

    Limit wods to track

--- src/assets/static/php/get-wods.php
+++ src/assets/static/php/get-wods.php
@@ -4,24 +4,24 @@ header('Content-Type: application/json');
 
 class API
 {
-    function GetWods()
-    {
-        $opts = array(
-            'http' => array(
-                'method' => "GET",
-                'header' => "Authorization: efdac3ad-1c14-4515-acdd-45320577b810\r\n" .
-                    "Content-Type: application/json\r\n"
-            )
-        );
+  function GetWods()
+  {
+    $opts = array(
+      'http' => array(
+        'method' => "GET",
+        'header' => "Authorization: efdac3ad-1c14-4515-acdd-45320577b810\r\n" .
+          "Content-Type: application/json\r\n"
+      )
+    );
 
-        $dates = $_GET['dates'];
-        $requestUrl = 'https://api.sugarwod.com/v2/workouts?dates=' . $dates;
+    $dates = $_GET['dates'];
+    $requestUrl = 'https://api.sugarwod.com/v2/workouts?track_id=GcLH3DKOFg&dates=' . $dates;
 
-        $context = stream_context_create($opts);
-        $wods = file_get_contents($requestUrl, false, $context);
+    $context = stream_context_create($opts);
+    $wods = file_get_contents($requestUrl, false, $context);
 
-        return $wods;
-    }
+    return $wods;
+  }
 }
 
 $API = new API;
